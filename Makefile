2c4f66525d12bb19789bb7124918a15a60ffbc40
TakuoIshida <archersparadox.100@gmail.com>
2024-09-28 17:12:16 +0900

    add user

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ build-db:
 	docker network create todo_app_nw || true
 	docker-compose -f docker-compose-db.yaml up -d
 
+run:
+	yarn dev
+
 
 code-gen:
 	yarn kysely-codegen --out-file src/database/types.ts
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,60 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { DB } from "./database/types";
+import { Kysely, MysqlDialect } from "kysely";
+import { zValidator } from "@hono/zod-validator";
+import { config } from "dotenv";
+import { createPool } from "mysql2";
+import { z } from "zod";
+import { ulid } from "ulid";
+config();
+
+const dialect = new MysqlDialect({
+  pool: createPool({
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 7306,
+    connectionLimit: 10,
+  }),
+});
+
+export const db = new Kysely<DB>({
+  dialect,
+});
 
 const app = new Hono();
 
-app.get("/", (c) => {
+app.get("/", async (c) => {
+  const result = await db.selectFrom("users").selectAll().execute();
+  console.log("result", result);
   return c.text("Hello Hono!");
+});
+const schema = z.object({
+  name: z.string().min(3).max(255),
+  email: z.string().email(),
+});
+
+app.post("/users", zValidator("json", schema), async (c) => {
+  const data = c.req.valid("json");
+  console.log(data);
+  const newId = ulid();
+  const newUser = await db
+    .insertInto("users")
+    .values({
+      id: newId,
+      name: data.name,
+      email: data.email,
+    })
+    .execute();
+
+  console.log("newUser", newUser);
+  return c.json({
+    status: 201,
+    success: true,
+    message: `user: ${data.name} is created`,
+  });
 });
 
 const port = 4000;
